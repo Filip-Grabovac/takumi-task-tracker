5c2f3e3ed8259192289da22d8047308b9a356103
Milos <milos@Miloss-Laptop.local>
2025-01-20 15:46:00 +0100

    Update

--- src/custom.js
+++ src/custom.js
@@ -6,4 +6,4 @@ let apiMainUrl = 'https://x8ki-letl-twmt.n7.xano.io/api:Q7_040cb'
 
 let loginButton = document.querySelector('#login.submit.button')
 
-console.log((loginButton));
+console.log(('milos'));
