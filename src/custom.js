f491227f856ffc7adccc6dbeefafef9223997a17
Milos <milos@Miloss-Laptop.local>
2025-01-20 15:58:47 +0100

    Update

--- src/custom.js
+++ src/custom.js
@@ -6,4 +6,22 @@ let apiMainUrl = 'https://x8ki-letl-twmt.n7.xano.io/api:Q7_040cb'
 
 let loginButton = document.querySelector('.form-submit-color')
 
-console.log(('milos'));
+loginButton.addEventListener('click', (e) => {
+
+    e.preventDefault()
+
+    let email = document.querySelector('#Email').value
+    let papssword = document.querySelector('#Password').value
+
+    let requestBody = {
+        email,
+        papssword
+    }
+
+    let apiEndpoint = ('/auth/login')
+
+    fetch(apiMainUrl + apiEndpoint)
+
+})
+
+console.log(requestBody)
