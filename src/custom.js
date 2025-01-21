bd6b181184a67acbda8c0e563c6619637b23273e
Milos <milos@Miloss-Laptop.local>
2025-01-21 10:56:09 +0100

    Update

--- src/custom.js
+++ src/custom.js
@@ -31,7 +31,14 @@ loginButton.addEventListener('click', (e) => {
         .then((data) => {
             if (data.authToken) {
                 localStorage.setItem('authToken', data.authToken);
-                /* window.location.href = 'https://briliaton-com.webflow.io/admin-dashboard' */
+
+                if (data.role_id === 1) {
+                    window.location.href = 'briliaton-com.webflow.io/admin-dashboard'
+                } else {
+                    window.location.href = 'briliaton-com.webflow.io/user-dashboard'
+
+                }
+
             }
         });
 
