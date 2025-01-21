5eea30596be806a9c57c31d503f26c66d8873140
Milos <milos@Miloss-Laptop.local>
2025-01-21 12:20:05 +0100

    Update

---
+++ src/login.js
@@ -0,0 +1,52 @@
+
+'use strict';
+// LOGIN //
+
+let apiMainUrl = 'https://x8ki-letl-twmt.n7.xano.io/api:Q7_040cb'
+
+let loginButton = document.querySelector('.form-submit-btn')
+
+loginButton.addEventListener('click', (e) => {
+
+    e.preventDefault()
+
+    let email = document.querySelector('#Email').value
+    let password = document.querySelector('#Password').value
+
+    let requestBody = {
+        email,
+        password
+    }
+
+    let apiEndpoint = apiMainUrl + '/auth/login'
+
+    fetch(apiEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })
+        .then(response => response.json())
+        .then((data) => {
+            if (data.authToken) {
+                localStorage.setItem('authToken', data.authToken);
+
+                console.log(data)
+                console.log(data.role_id)
+                if (data.role_id === 1) {
+
+                    window.location.href = 'https://briliaton-com.webflow.io/admin-dashboard';
+                } else {
+                    window.location.href = 'https://briliaton-com.webflow.io/user-dashboard';
+
+                }
+
+            }
+        });
+
+})
+
+
+
+
