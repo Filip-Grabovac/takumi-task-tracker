7435231620b8942d0395b49f681f1d5b799d8585
Milos <milos@Miloss-Laptop.local>
2025-01-21 12:40:12 +0100

    Update

--- src/check-auth.js
+++ src/check-auth.js
@@ -1,23 +1,28 @@
 
 
-// Preuzimanje tokena iz local storage
-const authToken = localStorage.getItem('authToken');
+function checkAuth() {
 
-// Postavljanje URL-a API endpointa
-const apiUrl = 'https://x8ki-letl-twmt.n7.xano.io/api:Q7_040cb/auth/me';
 
-// Slanje GET zahteva sa tokenom u Authorization headeru
-fetch(apiUrl, {
-    method: 'GET',
-    headers: {
-        'Authorization': `Bearer ${authToken}`
-    }
-})
-    .then(response => response.json())
-    .then(data => {
-        // Prikazivanje odgovora sa servera
-        console.log(data);
+
+    // Preuzimanje tokena iz local storage
+    const authToken = localStorage.getItem('authToken');
+
+    // Postavljanje URL-a API endpointa
+    const apiUrl = 'https://x8ki-letl-twmt.n7.xano.io/api:Q7_040cb/auth/me';
+
+    // Slanje GET zahteva sa tokenom u Authorization headeru
+    fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${authToken}`
+        }
     })
-    .catch(error => {
-        console.error('Greška pri pozivanju API-ja:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            // Prikazivanje odgovora sa servera
+            return data;
+        })
+        .catch(error => {
+            console.error('Greška pri pozivanju API-ja:', error);
+        });
+}
