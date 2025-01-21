16f9851ac216c879574698f2f23c4af96d245b34
Milos <milos@Miloss-Laptop.local>
2025-01-21 11:02:53 +0100

    Update

--- src/custom.js
+++ src/custom.js
@@ -33,6 +33,7 @@ loginButton.addEventListener('click', (e) => {
                 localStorage.setItem('authToken', data.authToken);
 
                 if (data.role_id === 1) {
+
                     window.location.href = 'https://briliaton-com.webflow.io/admin-dashboard'
                 } else {
                     window.location.href = 'https://briliaton-com.webflow.io/user-dashboard'
@@ -43,5 +44,7 @@ loginButton.addEventListener('click', (e) => {
         });
 
 })
+
+console.log(data.role_id)
 
 
